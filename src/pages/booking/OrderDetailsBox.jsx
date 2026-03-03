eacca96de47ec29dda340780f927accc7c4599e3
basteen-Dev <basteen@digitfellas.com>
2026-03-03 11:39:22 +0530

    update

--- src/pages/booking/OrderDetailsBox.jsx
+++ src/pages/booking/OrderDetailsBox.jsx
@@ -59,7 +59,7 @@ export default function OrderDetailsBox({
                                         </div>
                                         <div className="text-xs text-gray-500 mt-1">
                                             {qty} ticket(s) •{' '}
-                                            {item.dateLabel || dayjs(item.booking_date).format('D MMMM YYYY') || item.booking_date}
+                                            {item.dateLabel || dayjs(item.booking_date).format('DD-MMM-YYYY') || item.booking_date}
                                             {item.slotLabel ? ` • ${item.slotLabel}` : ''}
                                         </div>
 
